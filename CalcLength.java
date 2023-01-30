ed0ce869f6f807caa0eae4dff9eda6efc41f9180
Madhura1510 <hardik.gohil@somaiya.edu>
2023-01-30 10:28:31 +0530

    Java file modified

--- CalcLength.java
+++ CalcLength.java
@@ -1,11 +1,16 @@
  import  java.util.*;
 class CalcLength {
     public static void main( String args[] ) {
-       Scanner sc =new Scanner(System.in);
+        System.out.println("Hello world");
+        Scanner sc =new Scanner(System.in);
         System.out.println("Enter a string:");
         String str=sc.nextLine();
+        
 
          
        int length = str.length();  
-       System.out.println("The length of the String \""+str+"\" is: " +length); }
+       System.out.println("The length of the String \""+str+"\" is: " +length); };
+
+       
+       
 }
