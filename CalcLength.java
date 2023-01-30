5a88ce57f34d86c083226bbdc470e2ad15a05b1e
hardikgohil28 <95288465+hardikgohil28@users.noreply.github.com>
2023-01-30 10:38:40 +0530

    error testing

--- CalcLength.java
+++ CalcLength.java
@@ -1,7 +1,7 @@
  import  java.util.*;
 class CalcLength {
     public static void main( String args[] ) {
-        System.out.println("Hello world");
+        System.out.println("Hello world")
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter a string:");
         String str=sc.nextLine();
